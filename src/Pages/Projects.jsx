82126cbc06cd52f871607fa9fc06ec55e4d2116d
JavohirMuradov <151539982+JavohirMuradov@users.noreply.github.com>
2024-06-05 18:56:03 +0500

    Update Projects.jsx

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -15,7 +15,7 @@ const Projects = () => {
   return (
     <main className='bg-primary text-secondary'>
       <section className="container flex flex-col items-center pt-10">
-        <h1 className="text-4xl relative after:absolute after:w-full after:h-[2px] after:bg-secondary after:scale-x-0 after:-bottom-2 after:left-0 hover:after:scale-x-100 after:duration-300">{t("section2.h1")}</h1>
+        <h1 className="text-4xl relative after:absolute after:w-full after:h-[2px] after:bg-secondary after:scale-x-0 after:-bottom-2 after:left-0 hover:after:scale-x-100 after:duration-300 cursor-pointer">{t("section2.h1")}</h1>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 my-10">
           {cardsData.map((card, index) => (
             <MyCard key={index} img={card.img} alt={card.alt} text={card.text} path={card.path} />
