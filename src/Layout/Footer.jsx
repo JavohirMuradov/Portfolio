6d631fd60ff73805b35a034f79f0087ac0ba2f59
JavohirMuradov <151539982+JavohirMuradov@users.noreply.github.com>
2024-06-05 18:50:50 +0500

    Update Footer.jsx

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -11,31 +11,37 @@ const Footer = () => {
       <div className="container flex flex-row justify-between border-t border-t-secondary h-full pt-5 items-center">
         <h1 className="font-julia text-5xl"><Link to={"/"}>Java</Link></h1>
         <div className="flex flex-row items-center gap-2">
-          <Link to={"tel:+998909653646"} target="_blank" rel="noreferrer">
-            <button className="bg-radial p-2 rounded-full dark:bg-dark-radial">
-              <FaPhone className="w-6 h-6 text-primary" color={colorTheme === "dark" ? "#9BEA9B" : "#4F4665"} />
-            </button>
-          </Link>
-          <Link to={"https://t.me/JavaMiyagi"} target="_blank" rel="noreferrer">
-            <button
-              className="bg-radial p-2 rounded-full dark:bg-dark-radial">
-              <FaTelegram className="w-6 h-6 text-primary" color={colorTheme === "dark" ? "#9BEA9B" : "#4F4665"} />
-            </button>
-          </Link>
-          <Link to={"https://github.com/JavohirMuradov"} target="_blank" rel="noreferrer">
-            <button
-              className="bg-radial p-2 rounded-full dark:bg-dark-radial">
-              <FaGithub className="w-6 h-6 text-primary" color={colorTheme === "dark" ? "#9BEA9B" : "#4F4665"} />
-            </button>
-          </Link>
-          <Link
-            to={"mailto:javohirmuradov030@gmail.com"}
-            target="_blank" rel="noreferrer">
-            <button
-              className="bg-radial p-2 rounded-full dark:bg-dark-radial">
-              <SiGmail className="w-6 h-6 text-primary" color={colorTheme === "dark" ? "#9BEA9B" : "#4F4665"} />
-            </button>
-          </Link>
+          <Link to={"/home"}>
+                        <button className="bg-custom-radial dark:bg-dark-custom-radial text-primary text-md py-2 px-4 rounded-full">
+                            {t("btn")}
+                        </button>
+                    </Link>
+                    <div className="flex flex-row items-center gap-2">
+                        <Link to={"tel:+998909653646"} target="_blank" rel="noreferrer">
+                            <button className="bg-radial p-2 rounded-full dark:bg-dark-radial">
+                                <FaPhone className="w-6 h-6 text-primary" color={colorTheme === "dark" ? "#9BEA9B" : "#4F4665"} />
+                            </button>
+                        </Link>
+                        <Link to={"https://t.me/JavaMiyagi"} target="_blank" rel="noreferrer">
+                            <button
+                                className="bg-radial p-2 rounded-full dark:bg-dark-radial">
+                                <FaTelegram className="w-6 h-6 text-primary" color={colorTheme === "dark" ? "#9BEA9B" : "#4F4665"} />
+                            </button>
+                        </Link>
+                        <Link to={"https://github.com/JavohirMuradov"} target="_blank" rel="noreferrer">
+                            <button
+                                className="bg-radial p-2 rounded-full dark:bg-dark-radial">
+                                <FaGithub className="w-6 h-6 text-primary" color={colorTheme === "dark" ? "#9BEA9B" : "#4F4665"} />
+                            </button>
+                        </Link>
+                        <Link
+                            to={"mailto:javohirmuradov030@gmail.com"}
+                            target="_blank" rel="noreferrer">
+                            <button
+                                className="bg-radial p-2 rounded-full dark:bg-dark-radial">
+                                <SiGmail className="w-6 h-6 text-primary" color={colorTheme === "dark" ? "#9BEA9B" : "#4F4665"} />
+                            </button>
+                        </Link>
         </div>
       </div>
     </footer>
